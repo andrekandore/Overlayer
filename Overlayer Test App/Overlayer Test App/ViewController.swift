30cce5ac96c8061b432efbb6180739c2dd60a6b9
andrekandore <pyunpyun@mac.com>
2016-07-16 23:59:53 +0900

    Added Simple Example

--- Overlayer Test App/Overlayer Test App/ViewController.swift
+++ Overlayer Test App/Overlayer Test App/ViewController.swift
@@ -7,19 +7,8 @@
 //
 
 import UIKit
+import Overlayer
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+class ViewController: OverlayableViewController {
 }
 
